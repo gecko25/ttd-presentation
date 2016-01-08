4d12e176fec64e9df4f29a47b803582294fa483d
saratankard <stankard@thelathe.com>
2016-01-08 14:23:59 -0500

    updating grunt to watch slides

--- presentation/Gruntfile.js
+++ presentation/Gruntfile.js
@@ -142,7 +142,7 @@ module.exports = function(grunt) {
 				tasks: 'css-core'
 			},
 			html: {
-				files: [ 'index.html'],
+				files: [ 'slides/*.html'],
 				tasks: 'processhtml'
 			},
 			markdown: {
