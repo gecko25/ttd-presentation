c1ce6d45a21e7a85597464c896a14118068b06dc
saratankard <stankard@thelathe.com>
2016-01-11 19:00:49 -0500

    add through slide 14

--- Resources/01-automated-testing/lib/test_case.js
+++ Resources/01-automated-testing/lib/test_case.js
@@ -1,5 +1,39 @@
 /*jslint indent: 2, plusplus: false, forin: true, onevar: false, eqeqeq: false*/
 /*globals assert, output*/
+
+
+/*modified for slide 14*/
+function testCase(testname, tests) {
+  var successful = 0;
+  var testCount = 0;
+
+  output("<h1>" + testname + "</h1>");
+  for (var test in tests) {
+    testCount++;
+
+    try {
+      tests[test]();
+      output(test, "#0c0");
+      successful++;
+    } catch (e) {
+      output(test + " failed: " + e.message, "#c00");
+    }
+  }
+
+  var color = successful == testCount ? "#0c0" : "#c00";
+
+  output("<strong>" + testCount + " tests, " +
+         (testCount - successful) + " failures</strong>",
+         color);
+}
+
+
+
+
+
+
+
+/* With setup & teardown
 function testCase(name, tests) {
   assert.count = 0;
   var successful = 0;
@@ -41,3 +75,4 @@ function testCase(name, tests) {
          (testCount - successful) + " failures</strong>",
          color);
 }
+*/
--- Resources/01-automated-testing/test/strftime_test.js
+++ Resources/01-automated-testing/test/strftime_test.js
@@ -10,7 +10,7 @@ console.log('%y:' + date.strftime('%y'));
 console.log('%F:' + date.strftime('%F'));
 */
 
-/*Part Two*/
+/*Part Two
 var date = new Date(1989, 0, 25);
 try{
   assert("%Y should return full year",
@@ -30,38 +30,39 @@ try{
 }catch(e){
   console.log("Test failed:" + e.message)
 }
+*/
 
 
 
-
-/*Part Three
+/*Part Three*/
+var date = new Date(1989, 0, 25);
 testCase("strftime test", {
-  setUp: function () {
+  /*setUp: function () {
     this.date = new Date(2009, 9, 2, 22, 14, 45);
-  },
+  },*/
 
   "test format specifier %Y": function () {
     assert("%Y should return full year",
-           Date.formats.Y(this.date) === 2009);
+           Date.formats.Y(date) === 1989);
   },
 
   "test format specifier %m": function () {
     assert("%m should return month",
-           Date.formats.m(this.date) === "10");
+           Date.formats.m(date) === "01");
   },
 
   "test format specifier %d": function () {
     assert("%d should return date",
-           Date.formats.d(this.date) === "02");
+           Date.formats.d(date) === "25");
   },
 
   "test format specifier %y": function () {
     assert("%y should return year as two digits",
-           Date.formats.y(this.date) === "09");
+           Date.formats.y(date) === "89");
   },
 
   "test format shorthand %F": function () {
     assert("%F should be shortcut for %Y-%m-%d",
            Date.formats.F === "%Y-%m-%d");
   }
-});*/
+});
